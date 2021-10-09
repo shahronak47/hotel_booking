7a748ab4f8642a428455413431c4975175b2349d
shahronak47 <shahronak47@yahoo.in>
2021-10-10 00:00:34 +0800

    added findings 2

--- server.R
+++ server.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(plotly)
+library(DT)
 
 server <- function(input, output) {
-  hotel_data <- read_csv('hotel_bookings.csv')
+  #hotel_data <- read_csv('hotel_bookings.csv')
   
   output$top10_monthly_tourist <- renderPlot({
     hotel_data %>%
@@ -42,4 +43,48 @@ server <- function(input, output) {
       theme(legend.position=c(.9,.75))
   })
   
+  
+  output$average_stay <- renderDataTable({
+    hotel_data %>%
+      mutate(`Number of children` = children + babies, 
+             total_time = stays_in_weekend_nights + stays_in_week_nights) %>%
+      group_by(`Number of children`) %>%
+      summarise(`Average Stay (in days)` = round(mean(total_time, na.rm = TRUE), 2), 
+                Count = n()) %>%
+      na.omit() -> res
+    
+    datatable(res, options = list(dom = 't'), rownames = NULL, selection = 'none')
+  })
+  
+  
+  output$average_stay_plot <- renderPlot({
+    hotel_data %>%
+      mutate(num = children + babies, 
+             total_time = stays_in_weekend_nights + stays_in_week_nights) %>%
+      group_by(num = factor(num)) %>%
+      summarise(avg_stay = round(mean(total_time, na.rm = TRUE), 2)) %>%
+      na.omit() %>%
+      ggplot(aes(num, avg_stay, group = 1)) + geom_line() + geom_point() + 
+      labs(title = "Average stay duration",
+           x = 'Number of children', 
+           y = 'Number of days') +
+      theme_bw()
+  })
+  
+  output$plot3 <- renderPlot({
+    hotel_data %>%
+      mutate(total = adults + children + babies) %>%
+      group_by(country) %>%
+      summarise(total = sum(total, na.rm = TRUE)) %>%
+      slice_max(total, n = 6) %>%
+      left_join(hotel_data, by = 'country') %>%
+      count(country, market_segment) %>%
+      group_by(country) %>%
+      mutate(n = prop.table(n) * 100) %>%
+      ggplot() + aes(market_segment, n) + 
+      geom_col() + facet_wrap(~country) + 
+      labs(x = 'Market Segment', y = 'Percentage of bookings', 
+           title = 'Which marketing segment is used by top 5 countries ?') + 
+      theme_bw()
+  })
 }
--- ui.R
+++ ui.R
@@ -3,14 +3,14 @@ library(shinydashboard)
 library(dashboardthemes)
 library(shinycssloaders)
 
-hotel_data <- readr::read_csv('hotel_bookings.csv')
+#hotel_data <- readr::read_csv('hotel_bookings.csv')
 #Get unique month-year combinations
-month_data <- unique(with(hotel_data, paste(arrival_date_year, arrival_date_month)))
+month_data <- unique(with(hotel_data, paste(arrival_date_month, arrival_date_year)))
 
 #Calculate valueBox statistics
 v1 <- nrow(hotel_data)
-v2 <- paste0(round(mean(hotel_data$is_canceled) * 100, 2), '%')
-v3 <- HTML(paste(month_data[1], month_data[length(month_data)], sep = '-<br>'))
+v2 <- HTML(c('<span style = "font-size : 20px">', paste(month_data[1], month_data[length(month_data)], sep = ' - '), '</span>'))
+v3 <- paste0(round(mean(hotel_data$is_canceled) * 100, 2), '%')
 v4 <- paste0(round(mean(hotel_data$children > 0 | hotel_data$babies > 0, na.rm = TRUE) * 100, 2), '%')
 
 ui <- dashboardPage(title = 'Hotel Booking',
@@ -25,7 +25,8 @@ ui <- dashboardPage(title = 'Hotel Booking',
   dashboardSidebar(sidebarMenu(
     #Declare menu sub items
     menuItem("Stats", tabName = "stats", icon = icon("sort-numeric-down-alt")),
-    menuItem("Findings", icon = icon("binoculars"), tabName = "findings")
+    menuItem("Findings1", icon = icon("binoculars"), tabName = "findings1"), 
+    menuItem("Findings2", icon = icon("binoculars"), tabName = "findings2")
   )),
   dashboardBody(
     shinyDashboardThemes(
@@ -36,12 +37,12 @@ ui <- dashboardPage(title = 'Hotel Booking',
       #Tab 1
       tabItem(tabName = "stats",
         valueBox(v1, 'Total data points', icon = icon('globe'), width = 3), 
-        valueBox(v2, 'Cancellation percentage', icon = icon('times'), width = 3, color = 'red'), 
-        valueBox(v3, 'Range of data', icon = icon('calendar'), width = 3, color = 'purple'), 
+        valueBox(v2, 'Range of data', icon = icon('calendar'), width = 3, color = 'purple'), 
+        valueBox(v3, 'Cancellation percentage', icon = icon('times'), width = 3, color = 'red'), 
         valueBox(v4, 'Visitors with kids', icon = icon('baby'), width = 3, color = 'green')
       ), 
       #Tab2
-      tabItem(tabName = "findings",
+      tabItem(tabName = "findings1",
         h2("Monthly Visits"),
         br(),
         h3('Top 10 countries to visit'),
@@ -66,6 +67,12 @@ ui <- dashboardPage(title = 'Hotel Booking',
               <li> At any given point city hotel has higher visitors than resort hotel ones. So if we are planning to start a bussiness we should focus opening a city hotel. </li>
             </ul>  
            '),
+      ), 
+      tabItem(tabName = "findings2",
+              plotOutput('plot3'),
+              br(), br(),
+              fluidRow(column(6, plotOutput('average_stay_plot', height = '300px')),
+                       column(6, dataTableOutput('average_stay', height = '300px')))
       )
     )
   )
