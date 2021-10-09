eedec3fa2d021ab2faed59bf93b9627c3b244ea2
shahronak47 <shahronak47@yahoo.in>
2021-10-09 21:11:16 +0800

    finishing tab 1

--- server.R
+++ server.R
@@ -1,6 +1,45 @@
 library(tidyverse)
+library(plotly)
 
 server <- function(input, output) {
-  #hotel_data <- read_csv('hotel_bookings.csv')
+  hotel_data <- read_csv('hotel_bookings.csv')
+  
+  output$top10_monthly_tourist <- renderPlot({
+    hotel_data %>%
+      dplyr::filter(arrival_date_month == input$month, is_canceled == 0) %>%
+      group_by(country, hotel) %>%
+      summarise(total_people = sum(adults + children + babies)) %>%
+      mutate(total = sum(total_people)) %>%
+      ungroup %>%
+      arrange(desc(total)) %>%
+      dplyr::filter(country %in% head(unique(country), 10)) %>%
+      mutate(country = factor(country, unique(country))) %>%
+      ggplot() + aes(country, total_people, fill = hotel) + 
+      geom_col(position = 'dodge') + 
+      labs(title = paste0('Number of visitors* in the month of ', input$month), 
+           x = 'Country', 
+           y = 'Count', 
+           caption = '*Visitors include adults and small childrens of all age') +
+      scale_fill_manual(values = c('red', 'blue'), name = 'Type of Hotel') + 
+      theme_bw() + 
+      theme(legend.position=c(.9,.75))
+  })
+  
+  output$plot2 <- renderPlot({
+    hotel_data %>%
+      dplyr::filter(is_canceled == 0) %>%
+      group_by(hotel, arrival_date_month) %>%
+      summarise(total_visitors = sum(adults + children + babies, na.rm = TRUE)) %>%
+      mutate(arrival_date_month = factor(arrival_date_month, month.name)) %>%
+      ggplot(aes(arrival_date_month, total_visitors, color = hotel, group = hotel)) + 
+      geom_line() + 
+      labs(title = "Number of visitors* by month",
+           x = 'Month', 
+           y = 'Count',
+           caption = '*Visitors include adults and small childrens of all age') +
+      scale_color_manual(values = c('red', 'blue'), name = 'Type of Hotel') + 
+      theme_bw() + 
+      theme(legend.position=c(.9,.75))
+  })
   
 }
--- ui.R
+++ ui.R
@@ -1,30 +1,71 @@
 library(shiny)
 library(shinydashboard)
 library(dashboardthemes)
+library(shinycssloaders)
+
+hotel_data <- readr::read_csv('hotel_bookings.csv')
+#Get unique month-year combinations
+month_data <- unique(with(hotel_data, paste(arrival_date_year, arrival_date_month)))
+
+#Calculate valueBox statistics
+v1 <- nrow(hotel_data)
+v2 <- paste0(round(mean(hotel_data$is_canceled) * 100, 2), '%')
+v3 <- HTML(paste(month_data[1], month_data[length(month_data)], sep = '-<br>'))
+v4 <- paste0(round(mean(hotel_data$children > 0 | hotel_data$babies > 0, na.rm = TRUE) * 100, 2), '%')
 
 ui <- dashboardPage(title = 'Hotel Booking',
+                    
   dashboardHeader(title = shinyDashboardLogo(
                     theme = "flat_red",
                     mainText = h3("Hotel Booking"),
                     boldText = "",
                     badgeText = ""
                   )),
+  
   dashboardSidebar(sidebarMenu(
+    #Declare menu sub items
     menuItem("Stats", tabName = "stats", icon = icon("sort-numeric-down-alt")),
     menuItem("Findings", icon = icon("binoculars"), tabName = "findings")
   )),
   dashboardBody(
     shinyDashboardThemes(
+      #Include the theme of the app
       theme = "blue_gradient"
     ),
     tabItems(
+      #Tab 1
       tabItem(tabName = "stats",
-        valueBox(nrow(hotel_data), 'Total bookings', icon = icon('globe'), width = 3), 
-        valueBox(paste0(round(mean(hotel_data$is_canceled) * 100, 2), '%'), 'Cancellation percentage', 
-                icon = icon('times'), width = 3, color = 'red')
+        valueBox(v1, 'Total data points', icon = icon('globe'), width = 3), 
+        valueBox(v2, 'Cancellation percentage', icon = icon('times'), width = 3, color = 'red'), 
+        valueBox(v3, 'Range of data', icon = icon('calendar'), width = 3, color = 'purple'), 
+        valueBox(v4, 'Visitors with kids', icon = icon('baby'), width = 3, color = 'green')
       ), 
+      #Tab2
       tabItem(tabName = "findings",
-        
+        h2("Monthly Visits"),
+        br(),
+        h3('Top 10 countries to visit'),
+        selectInput('month', 'Select Month : ', month.name),
+        br(),
+        withSpinner(plotOutput('top10_monthly_tourist'), type = 5),
+      HTML('<br/> <br/>
+            <ul> Observations - 
+              <br/>
+              <li> Overall, Portugal has the highest number of visitors. It is almost double than the visitors from the country at second place i.e Great Britain. So marketing team should focus on advertising more in Portugal than any other country. </li>
+              <li> There are more city hotel visitors than resort hotel ones. </li>
+            </ul>  
+           '), 
+          br(), 
+          br(), 
+          h3('Trend of visitors in different types of hotel by month'),
+          plotOutput('plot2'),
+      HTML('<br/> <br/>
+            <ul> Observations - 
+              <br/>
+              <li> It can be clearly seen from the plot that June - August are the hottest month for bookings. So maybe we can plan our launch around May so that we get initial traction of visitors</li>
+              <li> At any given point city hotel has higher visitors than resort hotel ones. So if we are planning to start a bussiness we should focus opening a city hotel. </li>
+            </ul>  
+           '),
       )
     )
   )
